171ba219315561f0a55c77a2ae9911341d7f8648
Univ-of-Tsukuba-Photo-Club-User <79183993+Univ-of-Tsukuba-Photo-Club-User@users.noreply.github.com>
2022-03-22 20:44:03 +0900

    Update slide-show.tsx

--- src/components/slide-show.tsx
+++ src/components/slide-show.tsx
@@ -25,9 +25,9 @@ const Slideshow: React.FC<Props> = (props) => {
         <img
           key={url}
           css={css({
-            width: "100vw"
+            width: "100vw",
             height: "100vh",
-            objectFit: "cover",
+            objectFit: "contain",
             position: "absolute",
             opacity: idx === urlIdx ? "1" : "0",
             transition: "opacity 1s ease",
