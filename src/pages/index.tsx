e05d470b72be7a6d53ce0d37240ca259ab0d38c9
Univ-of-Tsukuba-Photo-Club-User <79183993+Univ-of-Tsukuba-Photo-Club-User@users.noreply.github.com>
2022-03-22 23:03:59 +0900

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,29 +1,6 @@
-    if (typeof window !== "undefined") {
-      if (window.innerWidth > window.innerHeight) {
-        document.location = "https://tsukuba-photoclub.com/about/";
-      }
-    }
-
-import React from "react"
-import "./index.css"
-import "semantic-ui-css/semantic.min.css"
-import Slideshow from "../components/slide-show"
-import Header from "../components/header"
-import Meta from "../components/meta"
-
-const IndexPage: React.FC = () => (
-  <>
-    <Meta />
-    <Header fixed />
-    <Slideshow
-      imageUrls={[
-        "https://pbs.twimg.com/media/D3mc_PZV4AED70c?format=jpg&name=4096x4096",
-        "https://pbs.twimg.com/media/EIWfG8JXkAAV-KD?format=jpg&name=large",
-        "https://pbs.twimg.com/media/EIaE8zCXkAYsvlL?format=jpg&name=large",
-        "https://pbs.twimg.com/media/DN3xyoOUIAEb0sE?format=jpg&name=4096x4096"
-      ]}
-    />
-  </>
-)
-
-export default IndexPage
+if (typeof window !== "undefined") {
+  if (window.innerWidth < window.innerHeight) {
+    document.location = "https://tsukuba-photoclub.com/about/";
+  }else{
+    document.location = "https://tsukuba-photoclub.com/contact/";
+}
