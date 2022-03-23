0384c8791202f8539a0809ea52e9321c057a1463
Univ-of-Tsukuba-Photo-Club-User <79183993+Univ-of-Tsukuba-Photo-Club-User@users.noreply.github.com>
2022-03-23 09:26:24 +0900

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,28 @@
 import React from "react"
+import "./index.css"
+import "semantic-ui-css/semantic.min.css"
+import Slideshow from "../components/slide-show"
+import Header from "../components/header"
+import Meta from "../components/meta"
 
 if (typeof window !== "undefined") {
-  if (window.innerWidth < window.innerHeight) {
-    document.location = "/About";
-  }else{
+  if (window.innerWidth > window.innerHeight) {
     document.location = "/Contact";
   }
 }
 
 const IndexPage: React.FC = () => (
   <>
+    <Meta />
+    <Header fixed />
+    <Slideshow
+      imageUrls={[
+        "https://pbs.twimg.com/media/D3mc_PZV4AED70c?format=jpg&name=4096x4096",
+        "https://pbs.twimg.com/media/EIWfG8JXkAAV-KD?format=jpg&name=large",
+        "https://pbs.twimg.com/media/EIaE8zCXkAYsvlL?format=jpg&name=large",
+        "https://pbs.twimg.com/media/DN3xyoOUIAEb0sE?format=jpg&name=4096x4096"
+      ]}
+    />
   </>
 )
 
