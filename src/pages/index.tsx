4976f2400c3b36aa180e778419d7b88f1310cc8f
Univ-of-Tsukuba-Photo-Club-User <79183993+Univ-of-Tsukuba-Photo-Club-User@users.noreply.github.com>
2022-08-11 21:09:53 +0900

    Update index.tsx
    retry PageContainer

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,33 +2,22 @@ import React from "react"
 import "./index.css"
 import "semantic-ui-css/semantic.min.css"
 import Slideshow from "../components/slide-show"
-import Header from "../components/header"
+import PageContainer from "../components/page-container"
 import Meta from "../components/meta"
-import Container from "semantic-ui-react"
-import Footer from "../components/footer"
-import css from "@emotion/css"
 
 const IndexPage: React.FC = () => (
   <>
     <Meta />
-    <Header fixed />
-    <Container>
-      <div
-        css={css({
-          paddingTop: "12px",
-        })}
-      >
-        <Slideshow
-          imageUrls={[
-            "https://pbs.twimg.com/media/D3mc_PZV4AED70c?format=jpg&name=4096x4096",
-            "https://pbs.twimg.com/media/EIWfG8JXkAAV-KD?format=jpg&name=large",
-            "https://pbs.twimg.com/media/EIaE8zCXkAYsvlL?format=jpg&name=large",
-            "https://pbs.twimg.com/media/DN3xyoOUIAEb0sE?format=jpg&name=4096x4096"
-          ]}
-        />
-      </div>
-    </Container>
-    <Footer />
+    <PageContainer>
+      <Slideshow
+        imageUrls={[
+          "https://pbs.twimg.com/media/D3mc_PZV4AED70c?format=jpg&name=4096x4096",
+          "https://pbs.twimg.com/media/EIWfG8JXkAAV-KD?format=jpg&name=large",
+          "https://pbs.twimg.com/media/EIaE8zCXkAYsvlL?format=jpg&name=large",
+          "https://pbs.twimg.com/media/DN3xyoOUIAEb0sE?format=jpg&name=4096x4096"
+        ]}
+      />
+    </PageContainer>
   </>
 )
 
