bc5cbad4febe608d85725698ab50c8b906a78a13
Univ-of-Tsukuba-Photo-Club-User <79183993+Univ-of-Tsukuba-Photo-Club-User@users.noreply.github.com>
2022-08-11 21:21:41 +0900

    Update index.tsx
    div

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,19 +4,26 @@ import "semantic-ui-css/semantic.min.css"
 import Slideshow from "../components/slide-show"
 import PageContainer from "../components/page-container"
 import Meta from "../components/meta"
+import css from "@emotion/css"
 
 const IndexPage: React.FC = () => (
   <>
     <Meta />
     <PageContainer>
-      <Slideshow
-        imageUrls={[
-          "https://pbs.twimg.com/media/D3mc_PZV4AED70c?format=jpg&name=4096x4096",
-          "https://pbs.twimg.com/media/EIWfG8JXkAAV-KD?format=jpg&name=large",
-          "https://pbs.twimg.com/media/EIaE8zCXkAYsvlL?format=jpg&name=large",
-          "https://pbs.twimg.com/media/DN3xyoOUIAEb0sE?format=jpg&name=4096x4096"
-        ]}
-      />
+      <div
+        css={css({
+          align: "center"
+        })}
+      >
+        <Slideshow
+          imageUrls={[
+            "https://pbs.twimg.com/media/D3mc_PZV4AED70c?format=jpg&name=4096x4096",
+            "https://pbs.twimg.com/media/EIWfG8JXkAAV-KD?format=jpg&name=large",
+            "https://pbs.twimg.com/media/EIaE8zCXkAYsvlL?format=jpg&name=large",
+            "https://pbs.twimg.com/media/DN3xyoOUIAEb0sE?format=jpg&name=4096x4096"
+          ]}
+        />
+      </div>
     </PageContainer>
   </>
 )
