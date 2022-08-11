24dda3f27c6ca1db31aa44ba972339f4007fbef2
Univ-of-Tsukuba-Photo-Club-User <79183993+Univ-of-Tsukuba-Photo-Club-User@users.noreply.github.com>
2022-08-11 23:59:20 +0900

    Update index.tsx
    revive div and add left: "0px"

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,18 +10,22 @@ const IndexPage: React.FC = () => (
   <>
     <Meta />
     <PageContainer>
-      <Slideshow
+      <div
         css={css({
           width: "100vw",
-          height: "67vw"
+          height: "67vw",
+          left: "0px"
         })}
-        imageUrls={[
-          "https://pbs.twimg.com/media/D3mc_PZV4AED70c?format=jpg&name=4096x4096",
-          "https://pbs.twimg.com/media/EIWfG8JXkAAV-KD?format=jpg&name=large",
-          "https://pbs.twimg.com/media/EIaE8zCXkAYsvlL?format=jpg&name=large",
-          "https://pbs.twimg.com/media/DN3xyoOUIAEb0sE?format=jpg&name=4096x4096"
-        ]}
-      />
+      >
+        <Slideshow
+          imageUrls={[
+            "https://pbs.twimg.com/media/D3mc_PZV4AED70c?format=jpg&name=4096x4096",
+            "https://pbs.twimg.com/media/EIWfG8JXkAAV-KD?format=jpg&name=large",
+            "https://pbs.twimg.com/media/EIaE8zCXkAYsvlL?format=jpg&name=large",
+            "https://pbs.twimg.com/media/DN3xyoOUIAEb0sE?format=jpg&name=4096x4096"
+          ]}
+        />
+      </div>
     </PageContainer>
   </>
 )
