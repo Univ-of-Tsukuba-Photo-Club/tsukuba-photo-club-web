5a3897290a5eced18134eb5c37ea305dd2da46a8
Univ-of-Tsukuba-Photo-Club-User <79183993+Univ-of-Tsukuba-Photo-Club-User@users.noreply.github.com>
2022-08-11 20:37:34 +0900

    Update index.tsx
    <div>children</div>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,14 +10,20 @@ const IndexPage: React.FC = () => (
   <>
     <Meta />
     <Header fixed />
-    <Slideshow
-      imageUrls={[
-        "https://pbs.twimg.com/media/D3mc_PZV4AED70c?format=jpg&name=4096x4096",
-        "https://pbs.twimg.com/media/EIWfG8JXkAAV-KD?format=jpg&name=large",
-        "https://pbs.twimg.com/media/EIaE8zCXkAYsvlL?format=jpg&name=large",
-        "https://pbs.twimg.com/media/DN3xyoOUIAEb0sE?format=jpg&name=4096x4096"
-      ]}
-    />
+    <div
+      css={css({
+        paddingTop: "12px",
+      })}
+    >
+      <Slideshow
+        imageUrls={[
+          "https://pbs.twimg.com/media/D3mc_PZV4AED70c?format=jpg&name=4096x4096",
+          "https://pbs.twimg.com/media/EIWfG8JXkAAV-KD?format=jpg&name=large",
+          "https://pbs.twimg.com/media/EIaE8zCXkAYsvlL?format=jpg&name=large",
+          "https://pbs.twimg.com/media/DN3xyoOUIAEb0sE?format=jpg&name=4096x4096"
+        ]}
+      />
+    </div>
     <Footer />
   </>
 )
