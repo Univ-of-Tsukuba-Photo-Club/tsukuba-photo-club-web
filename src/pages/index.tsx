361c896506c15b2e59c817e511229f8f790948a0
Univ-of-Tsukuba-Photo-Club-User <79183993+Univ-of-Tsukuba-Photo-Club-User@users.noreply.github.com>
2022-08-11 20:33:10 +0900

    Update index.tsx
    restore to previous revision

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,22 +2,23 @@ import React from "react"
 import "./index.css"
 import "semantic-ui-css/semantic.min.css"
 import Slideshow from "../components/slide-show"
-import PageContainer from "../components/page-container"
+import Header from "../components/header"
 import Meta from "../components/meta"
+import Footer from "../components/footer"
 
 const IndexPage: React.FC = () => (
   <>
     <Meta />
-    <PageContainer>
-      <Slideshow
-        imageUrls={[
-          "https://pbs.twimg.com/media/D3mc_PZV4AED70c?format=jpg&name=4096x4096",
-          "https://pbs.twimg.com/media/EIWfG8JXkAAV-KD?format=jpg&name=large",
-          "https://pbs.twimg.com/media/EIaE8zCXkAYsvlL?format=jpg&name=large",
-          "https://pbs.twimg.com/media/DN3xyoOUIAEb0sE?format=jpg&name=4096x4096"
-        ]}
-      />
-    </PageContainer>
+    <Header fixed />
+    <Slideshow
+      imageUrls={[
+        "https://pbs.twimg.com/media/D3mc_PZV4AED70c?format=jpg&name=4096x4096",
+        "https://pbs.twimg.com/media/EIWfG8JXkAAV-KD?format=jpg&name=large",
+        "https://pbs.twimg.com/media/EIaE8zCXkAYsvlL?format=jpg&name=large",
+        "https://pbs.twimg.com/media/DN3xyoOUIAEb0sE?format=jpg&name=4096x4096"
+      ]}
+    />
+    <Footer />
   </>
 )
 
