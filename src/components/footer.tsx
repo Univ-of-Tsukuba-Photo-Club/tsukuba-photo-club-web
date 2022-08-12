e6f5cb3c7bcf23fbeac6ccc35a4381ac2ce9a9e8
Univ-of-Tsukuba-Photo-Club-User <79183993+Univ-of-Tsukuba-Photo-Club-User@users.noreply.github.com>
2022-08-12 15:19:55 +0900

    Update footer.tsx
    fix mistake

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -19,8 +19,8 @@ const Footer: React.FC = () => (
       <p>
         ©︎ 2020 筑波大学写真部
         <div css={css({ fontSize: "10px", marginBottom: "20px !important" })}>
-          <span css={css({ display: inline-block })}>当サイトでは、Google Analyticsを使用してアクセス情報の</span>
-          <span css={css({ display: inline-block })}>収集・処理を行っています。詳しくは
+          <span css={css({ display: "inline-block" })}>当サイトでは、Google Analyticsを使用してアクセス情報の</span>
+          <span css={css({ display: "inline-block" })}>収集・処理を行っています。詳しくは
           <a
             href="https://policies.google.com/technologies/partner-sites?hl=ja"
             target="_blank"
