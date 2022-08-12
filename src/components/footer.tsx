ab87b56f38d17d2dcce409431b79ed58e404449f
Univ-of-Tsukuba-Photo-Club-User <79183993+Univ-of-Tsukuba-Photo-Club-User@users.noreply.github.com>
2022-08-12 15:37:36 +0900

    Update footer.tsx
    change span positions

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -19,8 +19,9 @@ const Footer: React.FC = () => (
       <p>
         ©︎ 2020 筑波大学写真部
         <div css={css({ fontSize: "10px", marginBottom: "20px !important" })}>
-          <span css={css({ display: "inline-block" })}>当サイトでは、Google Analyticsを使用してアクセス情報の</span>
-          <span css={css({ display: "inline-block" })}>収集・処理を行っています。詳しくは
+          <span css={css({ display: "inline-block" })}>当サイトでは、Google Analyticsを使用して</span>
+          <span css={css({ display: "inline-block" })}>アクセス情報の収集・処理を行っています。</span>
+          <span css={css({ display: "inline-block" })}>詳しくは
           <a
             href="https://policies.google.com/technologies/partner-sites?hl=ja"
             target="_blank"
