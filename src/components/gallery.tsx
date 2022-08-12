4b8eb5ee1cd78226b193f25b30c3be8a387d919b
Univ-of-Tsukuba-Photo-Club-User <79183993+Univ-of-Tsukuba-Photo-Club-User@users.noreply.github.com>
2022-08-12 22:10:34 +0900

    Create gallery.tsx

---
+++ src/components/gallery.tsx
@@ -0,0 +1,45 @@
+import React, { useEffect, useState, useRef } from "react"
+import { css } from "@emotion/core"
+
+type Props = {
+  imageUrls: string[]
+}
+
+const Gallery: React.FC<Props> = (props) => {
+  const { imageUrls } = props
+
+  const [urlIdx, setUrlIdx] = useState(0)
+
+  const ref = useRef({ imageUrls })
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      const isLast = ref.current.imageUrls.length - 1
+      setUrlIdx(urlIdx !== isLast ? urlIdx + 1 : 0)
+    }, 6000)
+    return () => clearTimeout(timer)
+  }, [urlIdx])
+
+  return (
+    <>
+      {imageUrls.map((url, idx) => (
+        <img
+          key={url}
+          css={css({
+            width: "80vw",
+            maxWidth: "80vh",
+            height: "80vw",
+            maxHeight: "80vh",
+            alignSelf: "center",
+            objectFit: "contain",
+            position: "absolute",
+            opacity: idx === urlIdx ? "1" : "0",
+            transition: "opacity 1s ease",
+          })}
+          src={url}
+        />
+      ))}
+    </>
+  )
+}
+
+export default Slideshow
