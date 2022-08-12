c86652ed892d86c1c1784794ee1d729d81cba1f2
Univ-of-Tsukuba-Photo-Club-User <79183993+Univ-of-Tsukuba-Photo-Club-User@users.noreply.github.com>
2022-08-12 11:44:41 +0900

    Update footer.tsx
    remove sns icons

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -18,9 +18,7 @@ const Footer: React.FC = () => (
     <div>
       <p>©︎ 2020 筑波大学写真部</p>
       <p css={css({ fontSize: "10px", marginBottom: "20px !important" })}>
-        当サイトではGoogle
-        Analyticsを使用してアクセス情報の収集、処理を行っています。
-        <br />
+        当サイトではGoogle Analyticsを使用してアクセス情報の収集、処理を行っています。
         詳しくは
         <a
           href="https://policies.google.com/technologies/partner-sites?hl=ja"
@@ -31,29 +29,6 @@ const Footer: React.FC = () => (
         </a>
         をご覧ください。
       </p>
-    </div>
-    <div css={css({ position: "absolute", right: "8px", marginBottom: "8px" })}>
-      <a
-        href="https://twitter.com/tsukuba_photo"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <Icon
-          name="twitter"
-          size="large"
-          color="black"
-          css={css({
-            marginRight: "8px !important",
-          })}
-        />
-      </a>
-      <a
-        href="https://www.instagram.com/tsukuba.photoclub/"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <Icon name="instagram" size="large" color="black" />
-      </a>
     </div>
   </div>
 )
