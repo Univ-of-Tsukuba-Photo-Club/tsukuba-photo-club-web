24577da6ec81401847580529c537e1d515a46810
Univ-of-Tsukuba-Photo-Club-User <79183993+Univ-of-Tsukuba-Photo-Club-User@users.noreply.github.com>
2022-08-12 19:09:20 +0900

    Update members.tsx
    restore

--- src/pages/members.tsx
+++ src/pages/members.tsx
@@ -64,11 +64,6 @@ const Members: React.FC = () => {
         <p css={css({ textAlign: "center", paddingBottom: "12px" })}>
           現在、追加中です。
         </p>
-        <div
-          css={css({
-            maxWidth: "80vw",
-          })}
-        >
         <Card.Group itemsPerRow={5} doubling stackable>
           {members.map(({ node }) => {
             return (
@@ -129,7 +124,6 @@ const Members: React.FC = () => {
             )
           })}
         </Card.Group>
-        </div>
       </PageContainer>
     </>
   )
