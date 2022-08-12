5a0adb924ed86c543d7edf28135afb3cb830c929
Univ-of-Tsukuba-Photo-Club-User <79183993+Univ-of-Tsukuba-Photo-Club-User@users.noreply.github.com>
2022-08-12 15:42:41 +0900

    Update footer.tsx
    change span positions

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -19,17 +19,21 @@ const Footer: React.FC = () => (
       <p>
         ©︎ 2020 筑波大学写真部
         <div css={css({ fontSize: "10px", marginBottom: "20px !important" })}>
-          <span css={css({ display: "inline-block" })}>当サイトでは、Google Analyticsを使用して</span>
-          <span css={css({ display: "inline-block" })}>アクセス情報の収集・処理を行っています。</span>
-          <span css={css({ display: "inline-block" })}>詳しくは
-          <a
-            href="https://policies.google.com/technologies/partner-sites?hl=ja"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            こちら
-          </a>
-          をご覧ください。</span>
+          <span css={css({ display: "inline-block" })}>
+            <span css={css({ display: "inline-block" })}>当サイトでは、Google Analyticsを使用して</span>
+            <span css={css({ display: "inline-block" })}>アクセス情報の収集・処理を行っています。</span>
+          </span>
+          <span css={css({ display: "inline-block" })}>
+            詳しくは
+            <a
+              href="https://policies.google.com/technologies/partner-sites?hl=ja"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              こちら
+            </a>
+            をご覧ください。
+          </span>
         </div>
       </p>
     </div>
