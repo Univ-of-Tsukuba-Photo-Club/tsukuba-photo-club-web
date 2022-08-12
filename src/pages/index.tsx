ff034023208bbb187672b0f7f0d58fb3e1fd4d10
Univ-of-Tsukuba-Photo-Club-User <79183993+Univ-of-Tsukuba-Photo-Club-User@users.noreply.github.com>
2022-08-12 09:22:59 +0900

    Update index.tsx
    fix mistake

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,15 +19,16 @@ const IndexPage: React.FC = () => (
       })}
     >
       <Header />
-        <Slideshow
-          imageUrls={[
-            "https://pbs.twimg.com/media/D3mc_PZV4AED70c?format=jpg&name=4096x4096",
-            "https://pbs.twimg.com/media/EIWfG8JXkAAV-KD?format=jpg&name=large",
-            "https://pbs.twimg.com/media/EIaE8zCXkAYsvlL?format=jpg&name=large",
-            "https://pbs.twimg.com/media/DN3xyoOUIAEb0sE?format=jpg&name=4096x4096"
-          ]}
-        />
+      <Slideshow
+        imageUrls={[
+          "https://pbs.twimg.com/media/D3mc_PZV4AED70c?format=jpg&name=4096x4096",
+          "https://pbs.twimg.com/media/EIWfG8JXkAAV-KD?format=jpg&name=large",
+          "https://pbs.twimg.com/media/EIaE8zCXkAYsvlL?format=jpg&name=large",
+          "https://pbs.twimg.com/media/DN3xyoOUIAEb0sE?format=jpg&name=4096x4096"
+        ]}
+      />
       <Footer />
+    </div>
   </>
 )
 
