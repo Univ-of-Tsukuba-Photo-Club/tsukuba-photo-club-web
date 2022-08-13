e4e5563cfc76d3d0aa9f2757b6fab2ef814a23b5
Univ-of-Tsukuba-Photo-Club-User <79183993+Univ-of-Tsukuba-Photo-Club-User@users.noreply.github.com>
2022-08-13 21:39:11 +0900

    Update gallery-photo.tsx

--- src/templates/gallery-photo.tsx
+++ src/templates/gallery-photo.tsx
@@ -1,16 +1,12 @@
-import React, { ReactNode } from "react"
+import React from "react"
+import PhotoContainer from "../components/photo-container"
 import Meta from "../components/meta"
-import AppHeader from "./header"
-import { Button, Container, Header, Icon, Divider } from "semantic-ui-react"
-import css from "@emotion/css"
-import ShareButtons from "../components/share-buttons"
-import Footer from "./footer"
 import { graphql, Link } from "gatsby"
+import css from "@emotion/css"
+import { Button, Header, Icon } from "semantic-ui-react"
+import ShareButtons from "../components/share-buttons"
 
 type Props = {
-  title?: string
-  children: ReactNode
-  text?: boolean
   data: any
   pageContext: any
   location: any
@@ -27,116 +23,80 @@ const GalleryPhotoTemplate: React.FC<Props> = (props) => {
         description={post.frontmatter.description}
         image={post.frontmatter.image?.publicURL}
       />
-      <div
-        css={css({
-          display: "flex",
-          flexFlow: "column",
-          minHeight: "100vh",
-          height: "100%",
-        })}
-      >
-        <AppHeader />
-        <Container
-          text=true
-          css={css`
-            @media (max-width: 800px) {
-              padding-top: 32px;
-            }
-            padding-top: 64px;
-            flex: 1 0 auto;
-          `}
+      <PhotoContainer>
+        <Header
+          as="h1"
+          css={css({
+            fontFamily: "sans-serif !important;",
+          })}
         >
-          {title ? (
-            <>
-              <Header as="h1" textAlign="center">
-                {title}
-              </Header>
-              <Divider />
-            </>
-          ) : (
-            <></>
-          )}
-          <div
-            css={css({
-              paddingTop: "12px",
-            })}
-          >
-            <Header
-              as="h1"
-              css={css({
-                fontFamily: "sans-serif !important;",
-              })}
-            >
-              {post.frontmatter.title}
-            </Header>
-            <div
-              css={css({
-                color: "grey",
-                marginBottom: "20px",
-              })}
-            >
-              {post.frontmatter.date}
-            </div>
-            <ShareButtons href={props.location.href} />
-            <span
-              css={css`
-                img {
-                  max-width: 100%;
-                  object-fit: contain;
-                }
-                p {
-                  line-height: 32px !important;
-                }
-              `}
-              dangerouslySetInnerHTML={{ __html: post.html }}
-            />
-            <hr css={css({ marginTop: "24px" })} />
-            <ul
-              style={{
-                display: `flex`,
-                flexWrap: `wrap`,
-                justifyContent: `space-between`,
-                listStyle: `none`,
-                padding: 0,
-              }}
-            >
-              <li>
-                {previous && (
-                  <Button
-                    icon
-                    labelPosition="left"
-                    as={Link}
-                    to={previous.fields.slug}
-                    style={{
-                      margin: "4px",
-                    }}
-                  >
-                    <Icon name="arrow left" />
-                    {previous.frontmatter.title}
-                  </Button>
-                )}
-              </li>
-              <li>
-                {next && (
-                  <Button
-                    icon
-                    labelPosition="right"
-                    as={Link}
-                    to={next.fields.slug}
-                    style={{
-                      margin: "4px",
-                    }}
-                  >
-                    <Icon name="arrow right" />
-                    {next.frontmatter.title}
-                  </Button>
-                )}
-              </li>
-            </ul>
-          </div>
-        </Container>
-        <Footer />
-      </div>
+          {post.frontmatter.title}
+        </Header>
+        <div
+          css={css({
+            color: "grey",
+            marginBottom: "20px",
+          })}
+        >
+          {post.frontmatter.date}
+        </div>
+        <ShareButtons href={props.location.href} />
+        <span
+          css={css`
+            img {
+              max-width: 100%;
+              object-fit: contain;
+            }
+            p {
+              line-height: 32px !important;
+            }
+          `}
+          dangerouslySetInnerHTML={{ __html: post.html }}
+        />
+        <hr css={css({ marginTop: "24px" })} />
+        <ul
+          style={{
+            display: `flex`,
+            flexWrap: `wrap`,
+            justifyContent: `space-between`,
+            listStyle: `none`,
+            padding: 0,
+          }}
+        >
+          <li>
+            {previous && (
+              <Button
+                icon
+                labelPosition="left"
+                as={Link}
+                to={previous.fields.slug}
+                style={{
+                  margin: "4px",
+                }}
+              >
+                <Icon name="arrow left" />
+                {previous.frontmatter.title}
+              </Button>
+            )}
+          </li>
+          <li>
+            {next && (
+              <Button
+                icon
+                labelPosition="right"
+                as={Link}
+                to={next.fields.slug}
+                style={{
+                  margin: "4px",
+                }}
+              >
+                <Icon name="arrow right" />
+                {next.frontmatter.title}
+              </Button>
+            )}
+          </li>
+        </ul>
+      </PhotoContainer>
     </>
   )
 }
