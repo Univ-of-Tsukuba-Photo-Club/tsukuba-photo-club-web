f1d9df3cf72f9ffc9f90a811a4606c673ff08548
Univ-of-Tsukuba-Photo-Club-User <79183993+Univ-of-Tsukuba-Photo-Club-User@users.noreply.github.com>
2022-08-14 20:34:09 +0900

    Update gallery-photo.tsx

--- src/templates/gallery-photo.tsx
+++ src/templates/gallery-photo.tsx
@@ -132,7 +132,7 @@ export const pageQuery = graphql`
       html
       frontmatter {
         title
-        date(formatString: "MMMM DD, YYYY")
+        name
         description
         image {
           publicURL
