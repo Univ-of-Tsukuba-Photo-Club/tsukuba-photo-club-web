bbcd0ff5fd68ef5f78cee5b45e95df50c76a63f8
Univ-of-Tsukuba-Photo-Club-User <79183993+Univ-of-Tsukuba-Photo-Club-User@users.noreply.github.com>
2022-08-14 11:58:14 +0900

    Update gallery-logo.tsx

--- src/components/gallery-logo.tsx
+++ src/components/gallery-logo.tsx
@@ -51,45 +51,6 @@ const GalleryLogo: React.FC<Props> = (props) => (
           </div>
         </div>
       </div>
-      <div
-        css={css` 
-          @media (max-aspect-ratio: 1/1) {
-            height: 0px;
-            display: none;
-          }
-        `}
-      >
-        <div
-          css={css({
-            backgroundColor: props.fixed
-              ? "rgba(255, 255, 255, 0.85)"
-              : "rgba(238, 238, 238, 0.25)",
-            position: props.fixed ? "fixed" : undefined,
-            width: "100vw",
-            zIndex: 1,
-            boxShadow: "0 1px 3px rgba(0,0,0,0.06)",
-          })}
-        >
-          <div
-            css={css`
-              width: 100%;
-              display: flex;
-              align-items: center;
-              flex-direction: column;
-            `}
-          >
-            <Link
-              to="/"
-              css={css`
-                margin-right: 0;
-                margin-left: 0;
-              `}
-            >
-              <Img fixed={data.file.childImageSharp.fixed} />
-            </Link>
-          </div>
-        </div>
-      </div>
     )}
   />
 )
