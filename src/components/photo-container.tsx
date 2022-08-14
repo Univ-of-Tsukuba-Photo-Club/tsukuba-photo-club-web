c234c4963381858ea7f8eddc22087005e8cfd17c
Univ-of-Tsukuba-Photo-Club-User <79183993+Univ-of-Tsukuba-Photo-Club-User@users.noreply.github.com>
2022-08-14 14:52:35 +0900

    Update photo-container.tsx

--- src/components/photo-container.tsx
+++ src/components/photo-container.tsx
@@ -49,7 +49,41 @@ const PageContainer: React.FC<Props> = ({ title, children, text = true }) => (
     >
       <GalleryHeaderMiddle />
     </div>
-    <Footer />
+    <div
+      css={css({
+        alignSelf: "center",
+        width: "100vw",
+        textAlign: "center",
+        display: "flex",
+        justifyContent: "center",
+        marginTop: "-8px",
+        marginBottom: "4px",
+        alignItems: "flex-end",
+      })}
+    >
+      <div>
+        <p>
+          ©︎ 2020 筑波大学写真部
+          <div css={css({ fontSize: "10px", marginBottom: "20px !important" })}>
+            <span css={css({ display: "inline-block" })}>
+              <span css={css({ display: "inline-block" })}>当サイトでは、Google Analyticsを使用して</span>
+              <span css={css({ display: "inline-block" })}>アクセス情報の収集・処理を行っています。</span>
+            </span>
+            <span css={css({ display: "inline-block" })}>
+              詳しくは
+              <a
+                href="https://policies.google.com/technologies/partner-sites?hl=ja"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                こちら
+              </a>
+              をご覧ください。
+            </span>
+          </div>
+        </p>
+      </div>
+    </div>
   </div>
 )
 
