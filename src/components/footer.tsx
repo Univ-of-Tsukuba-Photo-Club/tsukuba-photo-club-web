d3cced31318366d7337e5338f8e652fbb18f326f
Univ-of-Tsukuba-Photo-Club-User <79183993+Univ-of-Tsukuba-Photo-Club-User@users.noreply.github.com>
2022-08-14 13:30:26 +0900

    Update footer.tsx

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -6,7 +6,7 @@ const Footer: React.FC = () => (
   <div
     css={css({
       alignSelf: "center",
-      width: "100%",
+      width: "100vw",
       textAlign: "center",
       display: "flex",
       justifyContent: "center",
@@ -16,44 +16,26 @@ const Footer: React.FC = () => (
     })}
   >
     <div>
-      <p>©︎ 2020 筑波大学写真部</p>
-      <p css={css({ fontSize: "10px", marginBottom: "20px !important" })}>
-        当サイトではGoogle
-        Analyticsを使用してアクセス情報の収集、処理を行っています。
-        <br />
-        詳しくは
-        <a
-          href="https://policies.google.com/technologies/partner-sites?hl=ja"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          こちら
-        </a>
-        をご覧ください。
+      <p>
+        ©︎ 2020 筑波大学写真部
+        <div css={css({ fontSize: "10px", marginBottom: "20px !important" })}>
+          <span css={css({ display: "inline-block" })}>
+            <span css={css({ display: "inline-block" })}>当サイトでは、Google Analyticsを使用して</span>
+            <span css={css({ display: "inline-block" })}>アクセス情報の収集・処理を行っています。</span>
+          </span>
+          <span css={css({ display: "inline-block" })}>
+            詳しくは
+            <a
+              href="https://policies.google.com/technologies/partner-sites?hl=ja"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              こちら
+            </a>
+            をご覧ください。
+          </span>
+        </div>
       </p>
-    </div>
-    <div css={css({ position: "absolute", right: "8px", marginBottom: "8px" })}>
-      <a
-        href="https://twitter.com/tsukuba_photo"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <Icon
-          name="twitter"
-          size="large"
-          color="black"
-          css={css({
-            marginRight: "8px !important",
-          })}
-        />
-      </a>
-      <a
-        href="https://www.instagram.com/tsukuba.photoclub/"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <Icon name="instagram" size="large" color="black" />
-      </a>
     </div>
   </div>
 )
