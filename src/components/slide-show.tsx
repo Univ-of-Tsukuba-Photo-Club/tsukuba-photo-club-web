669becf8c1477289adcecd589ae12071ff2bed21
Univ-of-Tsukuba-Photo-Club-User <79183993+Univ-of-Tsukuba-Photo-Club-User@users.noreply.github.com>
2022-08-18 14:50:44 +0900

    Update slide-show.tsx

--- src/components/slide-show.tsx
+++ src/components/slide-show.tsx
@@ -6,7 +6,7 @@ type Props = {
 }
 
 const Slideshow: React.FC<Props> = (props) => {
-  const { imageUrls, linkUrl0, linkUrl0order, linkUrl1, linkUrl1order } = props
+  const { imageUrls, linkUrl } = props
 
   const [urlIdx, setUrlIdx] = useState(0)
 
@@ -20,31 +20,28 @@ const Slideshow: React.FC<Props> = (props) => {
   }, [urlIdx])
 
   return (
-    <>
-      <a
-        href={ urlIdx === linkUrl0order ? linkUrl0 : urlIdx === linkUrl1order ? linkUrl1 : "" }
-        tabindex={
-          linkUrl0 !== "" && urlIdx === linkUrl0order ? "" :
-          linkUrl1 !== "" && urlIdx === linkUrl1order ? "" : "-1"
-        }
-      >
-        {imageUrls.map((url, idx) => (
-          <img
-            key={url}
-            css={css({
-              width: "100vw",
-              maxHeight: "90vh",
-              alignSelf: "center",
-              objectFit: "contain",
-              position: "absolute",
-              opacity: idx === urlIdx ? "1" : "0",
-              transition: "opacity 1s ease",
-            })}
-            src={url}
-          />
-        ))}
-      </a>
-    </>
+    <a          
+      href={linkUrl}
+      css={css({
+        pointerEvents: linkUrl !== "" && urlIdx === 0 ? "" : "none",
+      })}
+    >
+      {imageUrls.map((url, idx) => (
+        <img
+          key={url}
+          css={css({
+            width: "100vw",
+            maxHeight: "90vh",
+            alignSelf: "center",
+            objectFit: "contain",
+            position: "absolute",
+            opacity: idx === urlIdx ? "1" : "0",
+            transition: "opacity 1s ease",
+          })}
+          src={url}
+        />
+      ))}
+    </a>
   )
 }
 
