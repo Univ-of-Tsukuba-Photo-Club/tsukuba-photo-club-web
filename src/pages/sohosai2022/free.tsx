aa512e66c87cd6cdec6fccacf4ab0ec002107b27
Univ-of-Tsukuba-Photo-Club-User <79183993+Univ-of-Tsukuba-Photo-Club-User@users.noreply.github.com>
2022-09-28 12:25:22 +0900

    Create free.tsx

---
+++ src/pages/sohosai2022/free.tsx
@@ -0,0 +1,78 @@
+import React from "react"
+import PageContainer from "../../components/page-container"
+import Meta from "../../components/meta"
+import { useStaticQuery, graphql, Link } from "gatsby"
+import { Card, CardGroup, Header } from "semantic-ui-react"
+import css from "@emotion/css"
+import Img from "gatsby-image"
+
+const Gallery: React.FC = () => {
+  const { allMarkdownRemark } = useStaticQuery(
+    graphql`
+      query {
+        allMarkdownRemark(
+          sort: { fields: [frontmatter___order], order: ASC }
+          filter: { fields: { collection: { eq: "gallery-sohosai2022-free" } } }
+        ) {
+          edges {
+            node {
+              excerpt
+              fields {
+                slug
+              }
+              frontmatter {
+                title
+                name
+                image {
+                  childImageSharp {
+                    fluid(maxWidth: 400, maxHeight: 400) {
+                      ...GatsbyImageSharpFluid_noBase64
+                    }
+                  }
+                }
+                description
+              }
+            }
+          }
+        }
+      }
+    `
+  )
+
+  const posts = allMarkdownRemark.edges as any[]
+
+  return (
+    <>
+      <Meta title="雙峰祭2022　自由展" />
+      <PageContainer title="雙峰祭2022　自由展" text={false}>
+        <Card.Group itemsPerRow={5} doubling stackable>
+          {posts.map(({ node }) => {
+            return (
+              <Card
+                css={css`
+                  @media (max-width: 750px) {
+                    pointer-events: none;
+                  }
+                `}
+                key={node.fields.slug}
+                as={Link}
+                to={node.fields.slug}
+              >
+                {node.frontmatter.image && (
+                  <Img fluid={node.frontmatter.image.childImageSharp.fluid} />
+                )}
+                <Card.Content>
+                  <Card.Header css={css({ textAlign: "center" })}>{node.frontmatter.title}</Card.Header>
+                  <Card.Meta css={css({ fontSize: "0.9em !important", textAlign: "center" })}>{node.frontmatter.name}</Card.Meta>
+                  <Card.Description css={css` @media (min-width: 750px) { display: none; } `}>{node.frontmatter.description}</Card.Description>
+                </Card.Content>
+              </Card>
+            )
+          })}
+        </Card.Group>
+      </PageContainer>
+    </>
+  )
+}
+
+export default Gallery
