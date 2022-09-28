e7751917d0ea1fe7e4ac48f2a06be04b7e0947d1
Univ-of-Tsukuba-Photo-Club-User <79183993+Univ-of-Tsukuba-Photo-Club-User@users.noreply.github.com>
2022-09-28 12:29:14 +0900

    Create theme.tsx

---
+++ src/pages/sohosai2022/theme.tsx
@@ -0,0 +1,70 @@
+import React from "react"
+import PageContainer from "../../components/page-container"
+import Meta from "../../components/meta"
+import { useStaticQuery, graphql, Link } from "gatsby"
+import { Card, CardGroup, Header } from "semantic-ui-react"
+import css from "@emotion/css"
+import Img from "gatsby-image"
+
+const Gallery: React.FC = () => {
+  const { allMarkdownRemark } = useStaticQuery(
+    graphql`
+      query {
+        allMarkdownRemark(
+          sort: { fields: [frontmatter___order], order: ASC }
+          filter: { fields: { collection: { eq: "gallery-sohosai2022-theme" } } }
+        ) {
+          edges {
+            node {
+              excerpt
+              fields {
+                slug
+              }
+              frontmatter {
+                image {
+                  childImageSharp {
+                    fluid(maxWidth: 400, maxHeight: 400) {
+                      ...GatsbyImageSharpFluid_noBase64
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    `
+  )
+
+  const posts = allMarkdownRemark.edges as any[]
+
+  return (
+    <>
+      <Meta title="雙峰祭2022　テーマ展「輝き」" />
+      <PageContainer title="雙峰祭2022　テーマ展「輝き」" text={false}>
+        <Card.Group itemsPerRow={5} doubling stackable>
+          {posts.map(({ node }) => {
+            return (
+              <Card
+                css={css`
+                  @media (max-width: 750px) {
+                    pointer-events: none;
+                  }
+                `}
+                key={node.fields.slug}
+                as={Link}
+                to={node.fields.slug}
+              >
+                {node.frontmatter.image && (
+                  <Img fluid={node.frontmatter.image.childImageSharp.fluid} />
+                )}
+              </Card>
+            )
+          })}
+        </Card.Group>
+      </PageContainer>
+    </>
+  )
+}
+
+export default Gallery
