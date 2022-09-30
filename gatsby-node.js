d720f1ead10e59ea46dfa09ceabc7dbccfdaec70
Univ-of-Tsukuba-Photo-Club-User <79183993+Univ-of-Tsukuba-Photo-Club-User@users.noreply.github.com>
2022-09-30 20:50:22 +0900

    Sohosai2022-0 (#150)
    Create formats for galleries; create main page and two galleries for Sohosai 2022

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,6 +5,7 @@ exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions
 
   const blogPost = path.resolve(`./src/templates/blog-post.tsx`)
+  const galleryPhoto = path.resolve(`./src/templates/gallery-photo.tsx`)
   const staticPage = path.resolve(`./src/templates/static-page.tsx`)
   const result = await graphql(
     `
@@ -52,6 +53,46 @@ exports.createPages = async ({ graphql, actions }) => {
       })
     })
 
+  const isSohosai2022Free = (page) => page.node.fields.slug.startsWith("/sohosai2022/free/")
+  pages
+    .filter((page) => isSohosai2022Free(page))
+    .forEach((post, index, posts) => {
+      const previous = index === posts.length - 1 ? null : posts[index + 1].node
+      const next = index === 0 ? null : posts[index - 1].node
+
+      createPage({
+        path: post.node.fields.slug,
+        component: galleryPhoto,
+        context: {
+          slug: post.node.fields.slug,
+          previous,
+          next,
+          gallerypath: "/sohosai2022/free",
+          galleryname: "雙峰祭2022　自由展",
+        },
+      })
+    })
+
+  const isSohosai2022Theme = (page) => page.node.fields.slug.startsWith("/sohosai2022/theme/")
+  pages
+    .filter((page) => isSohosai2022Theme(page))
+    .forEach((post, index, posts) => {
+      const previous = index === posts.length - 1 ? null : posts[index + 1].node
+      const next = index === 0 ? null : posts[index - 1].node
+
+      createPage({
+        path: post.node.fields.slug,
+        component: galleryPhoto,
+        context: {
+          slug: post.node.fields.slug,
+          previous,
+          next,
+          gallerypath: "/sohosai2022/theme",
+          galleryname: "雙峰祭2022　テーマ展",
+        },
+      })
+    })
+
   const isStatic = (page) => page.node.fields.static.startsWith("true")
   pages
     .filter((page) => isStatic(page))
@@ -74,6 +115,12 @@ exports.onCreateNode = ({ node, actions, getNode }) => {
     if (node.fileAbsolutePath.includes("content/blog")) {
       slug = `/blogs${slug}`
     }
+    if (node.fileAbsolutePath.includes("content/gallery-sohosai2022-free")) {
+      slug = `/sohosai2022/free${slug}`
+    }
+    if (node.fileAbsolutePath.includes("content/gallery-sohosai2022-theme")) {
+      slug = `/sohosai2022/theme${slug}`
+    }
 
     createNodeField({
       node,
@@ -92,6 +139,17 @@ exports.onCreateNode = ({ node, actions, getNode }) => {
       value: static,
     })
 
+    let static = "false"
+    if (node.fileAbsolutePath.includes("content/static")) {
+      static = "true"
+    }
+
+    createNodeField({
+      node,
+      name: "static",
+      value: static,
+    })
+    
     const parent = getNode(node.parent)
 
     createNodeField({
