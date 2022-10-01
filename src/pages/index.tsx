4bc5f28124caf95f4bf7d0c0d477a9b9249c4e2c
Univ-of-Tsukuba-Photo-Club-User <79183993+Univ-of-Tsukuba-Photo-Club-User@users.noreply.github.com>
2022-10-01 11:13:57 +0900

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -21,39 +21,21 @@ const IndexPage: React.FC = () => (
       <Header />
       <div
         css={css({
-          width: "100%",
+          maxWidth: "100%",
           maxHeight: "90vh",
         })}
       >
-        <div
-          css={css({
-            position: "relative",
-            width: "100%",
-            display: "block",
-            paddingTop: "75%",
-          })}
-        >
-          <div
-            css={css({
-              display: "block",
-              position: "absolute",
-              width: "100%",
-              height: "100%",
-            })}
-          >
-            <Slideshow
-              imageUrls={[
-                "https://pbs.twimg.com/media/FS89b-oaAAECgwp?format=jpg&name=large",
-                "https://pbs.twimg.com/media/D3mc_PZV4AED70c?format=jpg&name=4096x4096",
-                "https://pbs.twimg.com/media/EIWfG8JXkAAV-KD?format=jpg&name=large",
-                "https://pbs.twimg.com/media/EIaE8zCXkAYsvlL?format=jpg&name=large",
-                "https://pbs.twimg.com/media/DN3xyoOUIAEb0sE?format=jpg&name=4096x4096",
-                "https://raw.githubusercontent.com/Univ-of-Tsukuba-Photo-Club/tsukuba-photo-club-web/master/content/img/%E3%80%8C%E8%A6%8B%E3%82%8B%E3%80%8D%E5%86%99%E7%9C%9F%E3%81%8B%E3%82%89%E3%80%8C%E8%AA%AD%E3%82%80%E3%80%8D%E5%86%99%E7%9C%9F%E3%81%B8-1-.jpg"
-              ]}
-              linkUrl="/blogs/2022-06-21-2022年度新人展/"
-            />
-          </div>
-        </div>
+        <Slideshow
+          imageUrls={[
+            "https://pbs.twimg.com/media/FS89b-oaAAECgwp?format=jpg&name=large",
+            "https://pbs.twimg.com/media/D3mc_PZV4AED70c?format=jpg&name=4096x4096",
+            "https://pbs.twimg.com/media/EIWfG8JXkAAV-KD?format=jpg&name=large",
+            "https://pbs.twimg.com/media/EIaE8zCXkAYsvlL?format=jpg&name=large",
+            "https://pbs.twimg.com/media/DN3xyoOUIAEb0sE?format=jpg&name=4096x4096",
+            "https://raw.githubusercontent.com/Univ-of-Tsukuba-Photo-Club/tsukuba-photo-club-web/master/content/img/%E3%80%8C%E8%A6%8B%E3%82%8B%E3%80%8D%E5%86%99%E7%9C%9F%E3%81%8B%E3%82%89%E3%80%8C%E8%AA%AD%E3%82%80%E3%80%8D%E5%86%99%E7%9C%9F%E3%81%B8-1-.jpg"
+          ]}
+          linkUrl="/blogs/2022-06-21-2022年度新人展/"
+        />
       </div>
       <div
         css={css({
