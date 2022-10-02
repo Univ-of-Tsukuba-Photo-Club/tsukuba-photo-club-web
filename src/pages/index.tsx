cc8c20f974b99f8254d58de7ee031dbe9f130b0a
Univ-of-Tsukuba-Photo-Club-User <79183993+Univ-of-Tsukuba-Photo-Club-User@users.noreply.github.com>
2022-10-02 10:15:34 +0900

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -23,22 +23,18 @@ const IndexPage: React.FC = () => (
         css={css({
           height: "67vw",
           maxHeight: "90vh",
+          flex: "1 0 auto",
         })}
       >
         <Slideshow
           imageUrls={[
+            "/blackboard.jpg",
             "https://pbs.twimg.com/media/FS89b-oaAAECgwp?format=jpg&name=large",
             "https://pbs.twimg.com/media/DN3xyoOUIAEb0sE?format=jpg&name=4096x4096",
             "https://raw.githubusercontent.com/Univ-of-Tsukuba-Photo-Club/tsukuba-photo-club-web/master/content/img/%E3%80%8C%E8%A6%8B%E3%82%8B%E3%80%8D%E5%86%99%E7%9C%9F%E3%81%8B%E3%82%89%E3%80%8C%E8%AA%AD%E3%82%80%E3%80%8D%E5%86%99%E7%9C%9F%E3%81%B8-1-.jpg"
           ]}
           linkUrl="/blogs/2022-06-21-2022年度新人展/"
         />
-      </div>
-      <div
-        css={css({
-          flex: "1 0 auto",
-        })}
-      >
       </div>
       <Footer />
     </div>
