42f514d5204a2046087468551c2112549df7519d
Univ-of-Tsukuba-Photo-Club-User <79183993+Univ-of-Tsukuba-Photo-Club-User@users.noreply.github.com>
2022-10-02 11:35:25 +0900

    Update header.tsx

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -18,7 +18,7 @@ const Header: React.FC<Props> = (props) => (
             ? "rgba(255, 255, 255, 0.85)"
             : "rgba(238, 238, 238, 0.25)",
           position: props.fixed ? "fixed" : undefined,
-          width: "100vw",
+          width: "100%",
           zIndex: 1,
           boxShadow: "0 1px 3px rgba(0,0,0,0.06)",
         })}
@@ -60,6 +60,13 @@ const Header: React.FC<Props> = (props) => (
               }
             `}
           >
+            <Menu.Item name="about" as={Link} to="/about" />
+            <Menu.Item name="events" as={Link} to="/events" />
+            <Menu.Item name="blogs" as={Link} to="/blogs" />
+            <Menu.Item name="members" as={Link} to="/members" />
+            <Menu.Item name="join" as={Link} to="/2022年度新歓" />
+            <Menu.Item name="contact" as={Link} to="/contact" />
+            <Menu.Item name="twitter" as={Link} to="https://twitter.com/tsukuba_photo" target="blank" rel="noopener noreferrer" />
 {/*
 コメントアウト　静的ページを探し出しリンクを生成する機能
             {(data.allMarkdownRemark.edges as any[]).map(({ node }) => (
@@ -70,12 +77,6 @@ const Header: React.FC<Props> = (props) => (
               />
             ))}
 */}
-            <Menu.Item name="about" as={Link} to="/about" />
-            <Menu.Item name="blogs" as={Link} to="/blogs" />
-            <Menu.Item name="events" as={Link} to="/events" />
-            <Menu.Item name="members" as={Link} to="/members" />
-            <Menu.Item name="join" as={Link} to="/2022年度新歓" />
-            <Menu.Item name="twitter" as={Link} to="https://twitter.com/tsukuba_photo" target="blank" />
           </Menu>
         </div>
       </div>
@@ -89,7 +90,7 @@ const query = graphql`
   query {
     file(relativePath: { eq: "logo.png" }) {
       childImageSharp {
-        fixed(width: 300, height: 80) {
+        fixed(width: 250, height: 80) {
           ...GatsbyImageSharpFixed_noBase64
         }
       }
