9f701e1e47ef345852796362c07679f9812790f7
Univ-of-Tsukuba-Photo-Club-User <79183993+Univ-of-Tsukuba-Photo-Club-User@users.noreply.github.com>
2022-10-02 10:51:40 +0900

    Create gallery-header-middle.tsx

---
+++ src/components/gallery-header-middle.tsx
@@ -0,0 +1,48 @@
+import React from "react"
+import css from "@emotion/css"
+import { graphql, StaticQuery, Link } from "gatsby"
+import Img from "gatsby-image"
+
+type Props = {
+  fixed?: boolean
+}
+
+const GalleryHeaderMiddle: React.FC<Props> = (props) => (
+  <StaticQuery
+    query={query}
+    render={(data) => (
+      <div
+        css={css`
+          width: 100%;
+          display: flex;
+          align-items: center;
+          flex-direction: column;
+        `}
+      >
+        <Link
+          to="/"
+          css={css`
+            margin-right: 0;
+            margin-left: 0;
+          `}
+        >
+          <Img fixed={data.file.childImageSharp.fixed} loading="eager" />
+        </Link>
+      </div>
+    )}
+  />
+)
+
+export default GalleryHeaderMiddle
+
+const query = graphql`
+  query {
+    file(relativePath: { eq: "logo.png" }) {
+      childImageSharp {
+        fixed(width: 250, height: 80) {
+          ...GatsbyImageSharpFixed_noBase64
+        }
+      }
+    }
+  }
+`
