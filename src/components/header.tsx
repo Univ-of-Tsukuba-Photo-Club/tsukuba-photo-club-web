6f65a98fe4e81cfec286bb5fabfea75971950096
Univ-of-Tsukuba-Photo-Club-User <79183993+Univ-of-Tsukuba-Photo-Club-User@users.noreply.github.com>
2022-10-29 01:02:46 +0900

    Update header.tsx

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -64,6 +64,7 @@ const Header: React.FC<Props> = (props) => (
             <Menu.Item name="blogs" as={Link} to="/blogs" />
             <Menu.Item name="members" as={Link} to="/members" />
             <Menu.Item name="join" as={Link} to="/join" />
+            <Menu.Item name="contact" as={Link} to="/contact" />
             <Menu.Item name="twitter" as={Link} to="https://twitter.com/tsukuba_photo" target="blank" rel="noopener noreferrer" />
 {/*
 コメントアウト　静的ページを探し出しリンクを生成する機能
