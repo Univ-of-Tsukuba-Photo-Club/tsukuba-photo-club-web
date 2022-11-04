0dfe1923c775977a8bc8696421396823d0d65eb7
Univ-of-Tsukuba-Photo-Club-User <79183993+Univ-of-Tsukuba-Photo-Club-User@users.noreply.github.com>
2022-11-04 16:59:24 +0900

    slideshow

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,19 +4,47 @@ import "semantic-ui-css/semantic.min.css"
 import Slideshow from "../components/slide-show"
 import Header from "../components/header"
 import Meta from "../components/meta"
+import css from "@emotion/css"
+import Footer from "../components/footer"
 
 const IndexPage: React.FC = () => (
   <>
     <Meta />
-    <Header fixed />
-    <Slideshow
-      imageUrls={[
-        "https://pbs.twimg.com/media/D3mc_PZV4AED70c?format=jpg&name=4096x4096",
-        "https://pbs.twimg.com/media/EIWfG8JXkAAV-KD?format=jpg&name=large",
-        "https://pbs.twimg.com/media/EIaE8zCXkAYsvlL?format=jpg&name=large",
-        "https://pbs.twimg.com/media/DN3xyoOUIAEb0sE?format=jpg&name=4096x4096"
-      ]}
-    />
+    <div
+      css={css({
+        display: "flex",
+        flexFlow: "column",
+        minHeight: "100vh",
+        height: "100%",
+      })}
+    >
+      <Header />
+{/*
+2022年10月の更新までheaderで引数fixedを使用
+*/}
+      <div
+        css={css({
+          height: "67vw",
+          maxHeight: "90vh",
+          flex: "1 0 auto",
+        })}
+      >
+        <Slideshow
+          imageUrls={[
+            "/slideshow-blackboard.jpg",
+            "/slideshow-exhibits.jpg",
+            "/slideshow-photobooks.jpg",
+            "/slideshow-groupphoto.jpg"
+          ]}
+          linkUrl=""
+        />
+{/*
+imageUrls：スライドショーの写真　アスペクト比3:2限定・レポジトリ直下の/staticに保存
+linkUrl：URL（/aboutなどでよい）を入れると1枚目の時にスライドショーがそこへのリンクに変わる　イベント告知などでの使用を想定
+*/}
+      </div>
+      <Footer />
+    </div>
   </>
 )
 
